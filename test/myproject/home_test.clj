0913272575e107b9b8f08700b02fbc2621831ad0
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-09 21:18:14 +0100

    Add test

--- test/myproject/home_test.clj
+++ test/myproject/home_test.clj
@@ -1,5 +1,7 @@
 (ns myproject.home-test
   (:require [clj-http.client :as http]
+            [myproject.db :as db]
+            [myproject.queries :as queries]
             [clojure.test :refer :all]
             [hickory.core :as hickory]
             [hickory.select :as select]
@@ -16,14 +18,58 @@
 
 (deftest test-home-page-is-loaded-correctly
   (let [server (::server/server ig-extras/*test-system*)
+        db (::db/db ig-extras/*test-system*)
         url (reitit-extras/get-server-url server :host)
+        _ (queries/create-movie db {:title "The Matrix"
+                                    :year 1999
+                                    :director "Lana Wachowski, Lilly Wachowski"})
         body (-> (http/get url)
                  :body
                  (hickory/parse)
                  (hickory/as-hickory))]
-    (is (= "Clojure Stack Lite"
+    (is (= "Movies Lite"
            (->> body
-                (select/select (select/tag :span))
+                (select/select (select/tag :h1))
                 (first)
                 :content
-                (first))))))
+                (first))))
+    (is (= ["The Matrix" "1999" "Lana Wachowski, Lilly Wachowski"]
+          (->> body
+               (select/select (select/tag :td))
+               (map (comp first :content))
+               (butlast))))))
+
+(deftest test-create-movie-ok
+  (let [server (::server/server ig-extras/*test-system*)
+        db (::db/db ig-extras/*test-system*)
+        base-url (reitit-extras/get-server-url server :host)
+        url (str base-url "/movies")
+        {:keys [csrf-token cookies]} (test-utils/get-csrf-token-and-cookies base-url)]
+    ; Create a new movie
+    (http/post url {:cookies cookies
+                    :form-params {test-utils/CSRF-TOKEN-KEY csrf-token
+                                  :title "The Matrix"
+                                  :year 1999
+                                  :director "Lana Wachowski, Lilly Wachowski"}})
+
+    (is (= [{:director "Lana Wachowski, Lilly Wachowski"
+             :title "The Matrix"
+             :year 1999}]
+           (db/exec! db {:select [:title :year :director]
+                         :from [:movie]})))))
+
+(deftest test-delete-movie-ok
+  (let [server (::server/server ig-extras/*test-system*)
+        db (::db/db ig-extras/*test-system*)
+        base-url (reitit-extras/get-server-url server :host)
+        movie (queries/create-movie db {:title "The Matrix"
+                                        :year 1999
+                                        :director "Lana Wachowski, Lilly Wachowski"})
+        url (str base-url "/movies/" (:id movie))
+        {:keys [csrf-token cookies]} (test-utils/get-csrf-token-and-cookies base-url)]
+    ; Delete the movie
+    (http/delete url {:cookies cookies
+                      :headers {test-utils/CSRF-TOKEN-HEADER csrf-token}})
+
+    (is (= [] (db/exec! db {:select [:*]
+                            :from [:movie]})))))
