cb09a0ff0232c0972a3f79f3b5dc8ddb0aabfc48
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-05-14 23:16:46 +0100

    Add test for post register

--- test/myproject/auth_register_test.clj
+++ test/myproject/auth_register_test.clj
@@ -1,9 +1,11 @@
 (ns myproject.auth-register-test
   (:require [clj-http.client :as http]
             [clojure.test :refer :all]
+            [buddy.hashers :as hashers]
             [hickory.core :as hickory]
             [hickory.select :as select]
             [integrant-extras.tests :as ig-extras]
+            [myproject.db :as db]
             [myproject.server :as-alias server]
             [myproject.test-utils :as test-utils]
             [reitit-extras.tests :as reitit-extras]))
@@ -38,3 +40,21 @@
             :id "form-register"}
            (dissoc (->> body (select/select (select/tag :form)) first :attrs)
                    :class :hx-swap)))))
+
+(deftest test-post-register-ok
+  (let [server (::server/server ig-extras/*test-system*)
+        db (::db/db ig-extras/*test-system*)
+        base-url (reitit-extras/get-server-url server :host)
+        url (str base-url "/register")
+        {:keys [csrf-token cookies]} (test-utils/get-csrf-token-and-cookies url)
+        response (http/post url {:cookies cookies
+                                 :form-params {test-utils/CSRF-TOKEN-KEY csrf-token
+                                               :email "user@gmail.com"
+                                               :password "secret-password"}})
+        user (db/exec-one! db {:select [:email :password]
+                               :from [:user]})]
+
+    (is (= "user@gmail.com" (:email user)))
+    (is (true? (:valid (hashers/verify "secret-password" (:password user) {:alg :bcrypt+sha512}))))
+    (is (= 200 (:status response)))
+    (is (= "/" (get (:headers response) "HX-Redirect")))))
